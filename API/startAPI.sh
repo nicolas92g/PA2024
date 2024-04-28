05358b91b25a0a2a698c15f842c0e8d54c5dc4d1
Nicolas Guillot <nicolasguillot92@gmail.com>
2024-04-28 20:55:28 +0000

    docker compose fixes

--- API/startAPI.sh
+++ API/startAPI.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 test_connection() {
+    php artisan migrate >/dev/null 2>/dev/null
     php artisan migrate:status >/dev/null 2>&1
 }
 
+
 # Boucle pour tester la connexion
 while ! test_connection; do
-    echo "The database connection has failed. Retry in 1 second..."
-    sleep 1
+    echo "The database connection has failed. Retry in 4 second..."
+    sleep 4
 done
 
 php artisan db:seed
