69bdb5f84708d59b3096f429c8aa8d22a2e892d3
Nicolas GUILLOT <nicolasguillot92@gmail.com>
2024-04-30 18:16:33 +0200

    Update DatabaseSeeder.php

--- API/database/seeders/DatabaseSeeder.php
+++ API/database/seeders/DatabaseSeeder.php
@@ -49,9 +49,56 @@ class DatabaseSeeder extends Seeder
             'role' => 1
         ]);
 
+        DB::table('utilisateur')->insert([
+            'id' => 2,
+            'prenom' => 'user',
+            'nom' => 'benevole',
+            'mail' => 'benevole@gmail.com',
+            'mot_de_passe_hash' => 'aebb25da59d0a4aa3a7a807af30b63556d50f7c89703eb5ad7064be5b5150a8a',
+            'sel' => 'CKbcDWszB6',
+            'mail_verifie' => true,
+            'addresse' => 1
+        ]);
+
+        DB::table('est_un')->insert([
+            'utilisateur' => 2,
+            'role' => 2
+        ]);
+
+        DB::table('utilisateur')->insert([
+            'id' => 3,
+            'prenom' => 'nico',
+            'nom' => 'beneficiaire',
+            'mail' => 'beneficiaire@gmail.com',
+            'mot_de_passe_hash' => 'aebb25da59d0a4aa3a7a807af30b63556d50f7c89703eb5ad7064be5b5150a8a',
+            'sel' => 'CKbcDWszB6',
+            'mail_verifie' => true,
+            'addresse' => 1
+        ]);
+
         DB::table('type_activite')->insert([
             'id' => 1,
-            'nom' => ''
+            'nom' => 'Aide alimentaire'
+        ]);
+
+        DB::table('type_activite')->insert([
+            'id' => 2,
+            'nom' => 'Aide administratif'
+        ]);
+
+        DB::table('type_activite')->insert([
+            'id' => 3,
+            'nom' => 'Aide au transport'
+        ]);
+
+        DB::table('type_activite')->insert([
+            'id' => 4,
+            'nom' => 'Formations'
+        ]);
+
+        DB::table('type_activite')->insert([
+            'id' => 5,
+            'nom' => 'Autre'
         ]);
     }
 }
