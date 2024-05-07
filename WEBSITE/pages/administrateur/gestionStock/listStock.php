a3923e3b9f266750924300e8ac219ef336c8e045
Hydra75 <nicolasguillot92@gmail.com>
2024-05-07 14:43:41 +0200

    stock

--- WEBSITE/pages/administrateur/gestionStock/listStock.php
+++ WEBSITE/pages/administrateur/gestionStock/listStock.php
@@ -1,35 +1,52 @@
 <?php include_once("../../template.php"); ?>
+<?php include_once("../template.php"); ?>
 <!DOCTYPE html>
 <html class="h-100">
-<?=makeHead('Au Temps Donné - Intranet')?>
-<body class="cointainer-fluid d-flex h-100">
+    <?=makeHead('Au Temps Donné - Intranet')?>
+    <body class="cointainer-fluid d-flex h-100">
 
-<div class="bg-primary h-100 col-md-2 text-light d-flex flex-column align-items-center">
-    <div class="text-center mb-5">
-        <p>Nom : Votre Nom</p>
-        <p>Prénom : Votre Prénom</p>
-    </div>
+        <?=navbar(4, "..")?>
+        <div class="bg-secondary w-100 py-4">
+            <div class="d-flex justify-content-between">
+                <a class="btn btn-outline-primary m-3" href="addStock.php">Retour</a>
+                <h2>Liste des stocks</h2>
+                <div style="width: 100px"></div>
+            </div>
 
-    <div class="text-light d-flex flex-column align-items-center "style="margin-top: 50px;">
-        <a href="../home.php" class="btn btn-primary mb-5">
-            <i class="fas fa-home"></i> Gestion des bénévoles
-        </a>
-        <a href="../beneficiare/gestion_benef.php" class="btn btn-primary mb-5">
-            <i class="fas fa-calendar-alt"></i>  Gestion des bénéficiaires
-        </a>
-        <a href="../demande/gestion_demande.php" class="btn btn-primary mb-5">
-            <i class="fas fa-calendar-day"></i> Gestions des demandes
-        </a>
-        <a href="../activité/creation_activite.php" class="btn btn-primary mb-5">
-            <i class="fas fa-graduation-cap"></i> Créations des activités
-        </a>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">nom</th>
+                    <th scope="col">quantité</th>
+                    <th scope="col">description</th>
+                    <th scope="col">date limite</th>
+                    <th scope="col">fournisseur</th>
+                    <th scope="col">entrepot</th>
+                </tr>
+                </thead>
+                <tbody id="tableRows">
+                </tbody>
+            </table>
+        </div>
 
-        <a href="addStock.php" class="btn btn-secondary mb-5">
-            <i class="fas fa-graduation-cap"></i> Stocks
-        </a>
-
-        <a href="../profil.php" class="btn btn-primary">
-            <i class="fas fa-user-alt"></i> Profil
-        </a>
-    </div>
-</div>
+    </body>
+    <script>
+        getToApi('/product/list', null, getCookie('ATD-TOKEN')).then((response) => {
+            const table = document.getElementById('tableRows');
+            response.json().then((products) => {
+                console.log(products)
+                for (const product of products) {
+                    table.innerHTML += '<tr><th scope="row">' + product.id + '</th>' +
+                            '<td>' + product.nom + '</td>' +
+                            '<td>' + product.quantite + '</td>' +
+                            '<td>' + product.description + '</td>' +
+                            '<td>' + product.date_limite + '</td>' +
+                            '<td>' + product.fournisseur_nom + '</td>' +
+                            '<td>' + product.entrepot + '</td>' +
+                        '</tr>';
+                }
+            })
+        })
+    </script>
+</html>
