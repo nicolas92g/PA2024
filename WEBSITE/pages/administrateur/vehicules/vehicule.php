3d80b1c9553a6a135fde1df299594b4a5160a046
Lm2111 <123578221+Lm2111@users.noreply.github.com>
2024-05-09 00:27:35 +0200

    Update vehicule.php

--- WEBSITE/pages/administrateur/vehicules/vehicule.php
+++ WEBSITE/pages/administrateur/vehicules/vehicule.php
@@ -1,37 +1,15 @@
 <?php include_once("../../template.php"); ?>
+<?php include_once("../template.php"); ?>
 <!DOCTYPE html>
 <html class="h-100">
 <?=makeHead('Au Temps Donné - Intranet')?>
 <body class="cointainer-fluid d-flex h-100">
 
-<div class="bg-primary h-100 col-md-2 text-light d-flex flex-column align-items-center">
-    <div class="text-center mb-5">
-        <p>Nom : Votre Nom</p>
-        <p>Prénom : Votre Prénom</p>
-    </div>
+<?=navbar(5, "..")?>
 
-    <div class="text-light d-flex flex-column align-items-center "style="margin-top: 50px;">
-        <a href="../home.php" class="btn btn-primary mb-5">
-            <i class="fas fa-home"></i> Gestion des bénévoles
-        </a>
-        <a href="../beneficiare/gestion_benef.php" class="btn btn-primary mb-5">
-            <i class="fas fa-calendar-alt"></i>  Gestion des bénéficiaires
-        </a>
-        <a href="../demande/gestion_demande.php" class="btn btn-primary mb-5">
-            <i class="fas fa-calendar-day"></i> Gestions des demandes
-        </a>
-        <a href="../activité/creation_session_activite.php" class="btn btn-primary mb-5">
-            <i class="fas fa-graduation-cap"></i> Créations des activités
-        </a>
-        <a href="add_vehicule.php" class="btn btn-secondary mb-5 ">
-            <i class="fas fa-user-alt"></i> Véhicules
-        </a>
-        <a href="../profil.php" class="btn btn-primary">
-            <i class="fas fa-user-alt"></i> Profil
-        </a>
-    </div>
-</div>
 
+
+<div class="bg-secondary h-100 col-10 d-flex flex-column" id="parc-automobile">
 <div class="container-fluid">
     <div class="row">
         <div class="col-12 text-center">
@@ -54,7 +32,7 @@
         </div>
     </div>
 </div>
-
+</div>
 <script>
 
     function loadTruckData() {
@@ -67,7 +45,7 @@
             })
             .then(trucks => {
                 const tbody = document.getElementById('truckList');
-                tbody.innerHTML = ''; 
+                tbody.innerHTML = '';
                 trucks.forEach((truck, index) => {
                     const row = tbody.insertRow();
 
